d0cd9f7390b52b55bfbe5804caa9b03baf72d667
mstone3201 <96086149+mstone3201@users.noreply.github.com>
2022-10-31 13:23:33 -0500

    Added alternative depth hierarchy method
    Some systems don't support D3D12_FILTER_MINIMUM/D3D12_FILTER_MAXIMUM. Added an alternative method which uses gather and manually performs the max when building the depth hierarchy.

--- RIN/DepthMIPCS.hlsl
+++ RIN/DepthMIPCS.hlsl
@@ -1,3 +1,5 @@
+#define MIN_MAX_FILTER_SUPPORT
+
 struct Size {
 	uint2 size;
 };
@@ -8,6 +10,7 @@ RWTexture2D<float> childMIP : register(u0);
 SamplerState parentMIPSampler : register(s0);
 
 [RootSignature(
+#ifdef MIN_MAX_FILTER_SUPPORT
 	"RootFlags(0),"\
 	"RootConstants(num32BitConstants = 2, b0),"\
 	"DescriptorTable(SRV(t0)),"\
@@ -19,11 +22,30 @@ SamplerState parentMIPSampler : register(s0);
 		"addressV = TEXTURE_ADDRESS_CLAMP,"\
 		"addressW = TEXTURE_ADDRESS_CLAMP"\
 	")"
+#else
+	"RootFlags(0),"\
+	"RootConstants(num32BitConstants = 2, b0),"\
+	"DescriptorTable(SRV(t0)),"\
+	"DescriptorTable(UAV(u0)),"\
+	"StaticSampler("\
+		"s0,"\
+		"filter = FILTER_MIN_MAG_LINEAR_MIP_POINT,"\
+		"addressU = TEXTURE_ADDRESS_CLAMP,"\
+		"addressV = TEXTURE_ADDRESS_CLAMP,"\
+		"addressW = TEXTURE_ADDRESS_CLAMP"\
+	")"
+#endif
 )]
 [numthreads(32, 32, 1)]
 void main(
 	uint3 index : SV_DispatchThreadID
 ) {
-	if(index.x < childSize.size.x && index.y < childSize.size.y)
+	if(index.x < childSize.size.x && index.y < childSize.size.y) {
+	#ifdef MIN_MAX_FILTER_SUPPORT
 		childMIP[index.xy] = parentMIP.SampleLevel(parentMIPSampler, (float2(index.xy) + 0.5f) / childSize.size, 0);
+	#else
+		float4 depth = parentMIP.Gather(parentMIPSampler, (float2(index.xy) + 0.5f) / childSize.size);
+		childMIP[index.xy] = max(max(depth.x, depth.y), max(depth.z, depth.w));
+	#endif
+	}
 }
